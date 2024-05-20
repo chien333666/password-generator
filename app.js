00eb7a13662a079cd1425aa8d8300dd6c2675a0a
chien333666 <chien333@gmail.com>
2024-05-20 22:51:35 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 宣告靜態檔案位置
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send(`Hello world!`)
+  res.redirect('/movies')
 })
+
+app.get('/movies', (req, res) => {
+  res.send('listing movies')
+})
+
+app.get('/movie/:id', (req, res) => {
+  res.send(`showing movie ${req.params.id}`)
+})
+
 
 app.listen(port, () => {
   console.log(`1st express server on http://localhost:${port}`)
